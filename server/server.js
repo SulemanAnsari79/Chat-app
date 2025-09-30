501a7c8fc7dd5da89f8f9e82f02a9d2ddd06a1a9
Mohammad Suleman <official.suleman01@gmailcom>
2025-10-01 00:24:24 +0530

    Backend is in progress with basic and DB connection established

--- server/server.js
+++ server/server.js
@@ -2,14 +2,20 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import http from 'http';
+import { connectDB } from './lib/db.js';
 
+//Create express app with http
 const app=express();
 const server=http.createServer(app);//because socket.io support http
 
+//Middleware
 app.use(express.json({limit: "4mb"}));
 app.use(cors());//it allow all the url to connect with backend
 
 app.use("/api/status",(req,res)=>res.send("Server is live"));
+
+//Connect to db
+await connectDB();
 
 const PORT =process.env.PORT || 5000;
 
